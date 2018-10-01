81e3ada25184f14c7eb11675cae8699cf6a25b60
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-10-01 10:41:36 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,4 +1,4 @@
-<form action="" method="POST">
+<form action="prosesregis.php" method="POST">
     NIM : <input type="text" name="nim" pattern="\d*" maxlength="10" min="0" required>
     Nama : <input type="text" name="name" maxlength="25" required>
     Email : <input type="email" name="email" pattern="[a-z0-9._%+-]+@+gmail.+com" required>
