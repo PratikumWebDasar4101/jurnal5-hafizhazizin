ae0d438396c5c05fe50b294a8bcb9594a6dcd46c
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-10-01 09:49:32 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,31 @@
+<form action="" method="post">
+    <table>
+    <tr>
+         Nama : <input type="text" name="name" >
+    </tr>
+        <tr>
+            <td>Sematkan Komentar :</td>
+        </tr>
+        <tr>
+            <td><textarea name="komentar" id="" cols="30" rows="10"></textarea></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="kirim"></td>
+        </tr>
+    </table>
+</form>
+
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];
+        $komentar = $_POST['komentar'];
 
+        $count =strlen($komentar);
 
-
-
+        if($count < 5){
+            echo "komentar  harus lebih dari 5 ";
+        } else{
+            echo "jumlah kata :".$count;
+        }
+    }
 ?>
