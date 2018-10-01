d668156169be92ef1cc292d0a6b314cf1ac19453
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-10-01 10:37:30 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,11 @@
 <?php
+    if(isset($_POST['nim'])){
+        $nim = $_POST['nim'];
+        $nama = $_POST['name'];
+        $email = $_POST['email'];
 
-
-
-
-
+     echo $nim ."<br>"; 
+     echo $nama ."<br>";
+     echo $email ."<br>"; 
+    }
 ?>
